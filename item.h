1285fdb688bc528211979288431a58aeabd4bd3d
andrew <102703990+csandrew-dev@users.noreply.github.com>
2022-04-03 21:38:09 -0400

    created item class

---
+++ item.h
@@ -0,0 +1,20 @@
+#include "header.h"
+
+using namespace std;
+
+class item {
+   public: 
+      int getID(){
+        return id;
+      }
+      double getPrice(){
+        return price; 
+      }
+      void setPrice(double p){
+        price = p; 
+      }
+  
+  protected:
+    int id;
+    double price;
+}
