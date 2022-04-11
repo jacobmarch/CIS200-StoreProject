adab2c587951e7cc3128674f2c4874e0c9b7f19a
andrew <102703990+csandrew-dev@users.noreply.github.com>
2022-04-11 09:52:52 -0400

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,10 +1,12 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include<fstream>
 
 //classes
 #include "item.h"
 #include "customer.h"
 #include "transaction.h"
+#include "shopping.h"
 
 using namespace std;
