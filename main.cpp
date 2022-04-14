96bf408ec8ef8a7380716c763aefa4bd07c91822
Jacob Marchywka <dcfalcons2021@gmail.com>
2022-04-14 16:36:56 -0400

    Added cout statements to switch

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int input;
 //This variable can be used to track the user's input to access the different pieces of our program
 
+
 int main(){
   while (input != -1){
     cout << "What would you like to access?" << endl;
@@ -16,9 +17,29 @@ int main(){
     cin >> input;
     switch (input) {
       case 1:
+        cout << "Purchase Selected" << endl;
+        break;
       case 2:
+        cout << "Return Selected" << endl;
+        break;
       case 3:
+        cout << "Edit Item" << endl;
+        break;
       case 4:
+        cout << "Edit Inventory" << endl;
+        cout << "1) Add Inventory" << endl;
+        cout << "2) Remove Inventory" << endl;
+        cin >> input;
+        switch (input) {
+          case 1:
+            break;
+          case 2:
+            break;
+        }
+        break;
+      default:
+        cout << "Invalid Choice" << endl;
+        break;
 
     }
     
